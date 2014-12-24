ca4cfb606ad8564ca3b1fa6171b0541bb8a93122
Tomaz Wang <tomazwang.tw@gmail.com>
2014-12-24 19:37:20 +0000

    ajax-ex: update js file make it work and can do the callback funtion

--- ajax-ex/js/main-js.js
+++ ajax-ex/js/main-js.js
@@ -16,7 +16,7 @@ function createXmlHttpRequestObject(){
     }else{
         // for other Browser
         try{
-            xmlHttp = new XMLHttpRequset();
+            xmlHttp = new XMLHttpRequest();
         }catch(e){
             console.log(e);
             xmlHttp = false;
@@ -26,6 +26,7 @@ function createXmlHttpRequestObject(){
     if(!xmlHttp){
         alert("Something's wrong!! Can't create xmlhttpRequestObject");
     }else{
+        // console.log("xmlHttp create");
         return xmlHttp;
     }
     
@@ -34,9 +35,47 @@ function createXmlHttpRequestObject(){
 
 
 function process(){
+    console.log("entering process :"+xmlHttp.readyState);
     if(xmlHttp.readyState == 4 || xmlHttp.readyState === 0){
-        
+        // console.log("begin get input");
+        var textInput = encodeURIComponent(document.getElementById("inputBox").value);
+        xmlHttp.open("GET","./php/main-function.php?inputText="+textInput,true); // make a request
+        xmlHttp.onreadystatechange = handleServerResponse; // setting call back function
+        xmlHttp.send(null); // make a send, null for GET;
     }else{
-        
+        setTimeout(process(), 1000);
+        // wait and try again see if xmlHttp is a ready state.
     }
+}
+
+
+
+function handleServerResponse(){
+    // basically is a callback function
+    
+    // chech states first
+    if(xmlHttp.readyState == 4){
+        // done communicating
+        // console.log("Communication is done");    
+        if(xmlHttp.status == 200){
+            var xmlResponse = xmlHttp.responseXML;
+            // console.log(xmlResponse);
+            var resultText;
+            var e = xmlResponse.getElementsByTagName("resultText");
+            
+            for (var i=0;i<e.length;i++){
+                resultText = e[i].childNodes[0].nodeValue;
+            }
+            
+            
+            document.getElementById("letters_left").innerHTML = '<span style="color:blue">'+resultText+'</span>';
+            setTimeout( process(), 1000);
+        }else{
+            alert('Something went wrong!! Server response err.');
+        }
+    }
+    
+   
+    
+    
 }
