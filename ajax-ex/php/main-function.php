4c0bc612c26024d424d8ab961c7b301eafc48fdc
Tomaz Wang <tomazwang.tw@gmail.com>
2014-12-24 19:38:15 +0000

    ajax-ex: update main-function.php - make more child node for debugger. - count the letter left (not how many letters)

--- ajax-ex/php/main-function.php
+++ ajax-ex/php/main-function.php
@@ -1,16 +1,17 @@
 <?php
 
     header('Content-Type: text/xml');
-    echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"';
+    echo '<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>';
     
     
     echo '<response>';  // begining of response tag
     
     $inputText = $_GET['inputText'];
     $strlen = strlen($inputText);
+    echo '<getText>'.$inputText."</getText>";
     
     echo '<resultText>';
-    echo ''.$strlen.'letters left.'; 
+    echo ''.(140 - $strlen).' letters left.'; 
     echo '</resultText>';
     
     echo '<strlen>';
